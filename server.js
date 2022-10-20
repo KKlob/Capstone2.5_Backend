30365e02ad69a4842a3e677ab42cf48e3ffdd02b
kainklob <klobk23@gmail.com>
2022-10-20 14:27:16 -0400

    added cors middleware to API server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 
 // API ROUTES
 
